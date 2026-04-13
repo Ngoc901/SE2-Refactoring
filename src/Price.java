85d72cbecade8e5b6c1fa4d8c7ab44658bd18107
Ngoc901 <ngo.hua.24@lehre.mosbach.dhbw.de>
2026-04-13 19:14:00 +0200

    refactor: adjust frequent renter points for new releases

--- src/Price.java
+++ src/Price.java
@@ -37,4 +37,7 @@ class NewReleasePrice extends Price {
     double getCharge(int daysRented) {
         return daysRented * 3;
     }
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
 }
