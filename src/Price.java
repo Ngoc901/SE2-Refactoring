2e3bdf95f1fd5eaa2d9d2febbdb8a481568911ab
Ngoc901 <ngo.hua.24@lehre.mosbach.dhbw.de>
2026-04-13 16:02:00 +0200

    refactor: introduce a new Price class to handle different pricing rules

---
+++ src/Price.java
@@ -0,0 +1,40 @@
+abstract class Price {
+    abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
+}
+
+class RegularPrice extends Price {
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+    double getCharge(int daysRented) {
+        double charge = 2;
+        if (daysRented > 2)
+            charge += (daysRented - 2) * 1.5;
+        return charge;
+    }
+}
+
+class ChildrensPrice extends Price {
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+    double getCharge(int daysRented) {
+        double charge = 1.5;
+        if (daysRented > 3)
+            charge += (daysRented - 3) * 1.5;
+        return charge;
+    }
+}
+
+class NewReleasePrice extends Price {
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+}
