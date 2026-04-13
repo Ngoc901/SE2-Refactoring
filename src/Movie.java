c65568099cfb3f834e03520ad1904a4836d4ff45
Ngoc901 <ngo.hua.24@lehre.mosbach.dhbw.de>
2026-04-13 17:29:00 +0200

    refactor: use subclasses for each price type instead of a conditional

--- src/Movie.java
+++ src/Movie.java
@@ -3,42 +3,36 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price price;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     public String getTitle (){
         return title;
     }
     double getCharge(int daysRented) {
-        double charge = 0;
-        switch (priceCode) {
-            case REGULAR:
-                charge += 2;
-                if (daysRented > 2)
-                    charge += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case CHILDRENS:
-                charge += 1.5;
-                if (daysRented > 3)
-                    charge += (daysRented - 3) * 1.5;
-                break;
-        }
-        return charge;
+        return price.getCharge(daysRented);
     }
     int getFrequentRenterPoints(int daysRented) {
-        if (priceCode == NEW_RELEASE && daysRented > 1)
-            return 2;
-        return 1;
+        return price.getFrequentRenterPoints(daysRented);
     }
 }
